652cb686cb08ce9eeac3291e6c3092796e87bd64
unknown <sevendevl7@gmail.com>
2020-12-20 09:13:35 +0100

    add 3 helpers for front, create footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+    def container_card(title, &block)
+        content_tag(:div, class: "card card-accent-info mt-5 m-3 ml-3") do
+            content_tag(:div, class: "card-header") do
+                title
+            end +
+            content_tag(:div, class: "card-body") do
+                capture(&block)
+            end
+        end
+    end
+    
+    def boolean_display(boolean)
+        if boolean.nil?
+            content_tag(:span, "Non indiqué", class: "btn btn-default btn-xs")
+        elsif boolean == true
+            content_tag(:span, "Oui", class: "btn btn-success btn-xs")
+        else
+            content_tag(:span, "Non", class: "btn btn-danger btn-xs")
+        end
+    end
+    
+    # To display euros
+    def money_display(value)
+        return number_to_currency(value, unit: " €", separator: ",", delimiter: " ", precision: 0, format: "%n %u")
+    end
+    
 end
