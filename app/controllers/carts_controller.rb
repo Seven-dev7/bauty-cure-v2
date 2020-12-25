8c6c1c18d2bb82542f8d9f829b56a0d6dcdb3399
unknown <sevendevl7@gmail.com>
2020-12-25 13:44:03 +0100

    try to remove each produc from cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,5 +3,9 @@ class CartsController < ApplicationController
   def show
     @cart = Cart.find(params[:id])
   end
-
+  def delete_cart_item
+    @cart_product = CartProduct.find(params[:cart_id])
+    @cart_product.destroy_all  
+    redirect_to cart_path(current_user.cart.id)
+  end
 end
